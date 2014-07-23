b6087f5aa86f897b1e0a5a374d364283b238ccf7
mentos <mentos.name@gmail.com>
2014-07-23 19:21:22 +0400

    com 2

--- application/views/view_template.php
+++ application/views/view_template.php
@@ -108,53 +108,56 @@ $css_file_name = Less_Cache::Get( $files, $options );
         <div class="block">
             <div class="column span-8 col_bg">
                 <div class="photo">
-                    <a href="http://themes.muffingroup.com/tisson/page-builder/theme-options">
+                    <a href="/">
                         <img class="scale-with-grid" src="../../layout/img/content/enshtn.png" alt="Save plenty of time">
                     </a>
                 </div>
                 <div class="desc">
                     <h2>ТАБЛИЦЫ СТОИМОСТИ КОНТРАКТОВ</h2>
                     <p>Таблицы стоимости заказов (контрактов) игpока в преферанс.</p>
-                    <a class="" href="http://themes.muffingroup.com/tisson/page-builder/theme-options">Далее <i class="fa fa-angle-double-right"></i></a>
+                    <a class="" href="/">Далее <i class="fa fa-angle-double-right"></i></a>
                 </div>            
             </div>
 
             <div class="column span-8 col_bg">
                <div class="photo">
-                    <a href="http://themes.muffingroup.com/tisson/page-builder/theme-options">
+                    <a href="/">
                         <img class="scale-with-grid" src="../../layout/img/content/calc.png" alt="Save plenty of time">
                     </a>
                 </div>
                 <div class="desc">
                     <h2>КАЛЬКУЛЯТОР ПУЛИ</h2>
                     <p>Калькулятор расчета пули в преферансе (по «вистам»).</p>
-                    <a class="" href="http://themes.muffingroup.com/tisson/page-builder/theme-options">Далее <i class="fa fa-angle-double-right"></i></a>
+                    <a class="button" href="/">На четверых</a>
+                    <a class="left button" href="/">На троих</a>
                 </div>            
             </div>
 
             <div class="column span-8 last col_bg">
                <div class="photo">
-                    <a href="http://themes.muffingroup.com/tisson/page-builder/theme-options">
+                    <a href="/">
                         <img class="scale-with-grid" src="../../layout/img/content/kodex.png" alt="Save plenty of time">
                     </a>
                 </div>
                 <div class="desc">
                     <h2>КОДЕКС</h2>
                     <p>Кодекс предназначен для определения правильной процедуры игры в преферанс. </p>
-                    <a class="" href="http://themes.muffingroup.com/tisson/page-builder/theme-options">Далее <i class="fa fa-angle-double-right"></i></a>
+                    <a class="" href="/">Далее <i class="fa fa-angle-double-right"></i></a>
                 </div>             
             </div>
         </div>
+        <div class="content-container container-bg">
+        <p><b>Префера́нс</b> (фр. préférence — предпочтение, преимущество) — карточная игра со взятками. Получила распространение в России в середине XIX века. Предшественником преферанса считается вист. Играется втроём, вчетвером (в этом случае каждый игрок по очереди пропускает раздачу, что называется «сидит на прикупе») или вдвоём (преферанс вдвоём называют «гуса́рик»). Возможно играть составом и больше четырёх человек, но тогда игра теряет динамичность, и интерес к ней снижается, так как каждый игрок вынужден пропускать две и более раздач подряд. Часто вместо выражения «сыграть в преферанс» используется выражение «расписа́ть пу́льку».
+        <p>Преферанс является коммерческой игрой, то есть такой игрой на деньги, в которой результат в большей степени определяется умением игрока, нежели везением, в отличие от азартных игр. В преферансе деньги не являются элементом стратегии, что делает их необязательной частью игры.</p>
+        </div>
     </div>  
         
-    <footer id="footer">
+    <footer id="footer">        
         <div class="container">
-
-        </div>	
-        <div class="container">
-            <div class="column one copyrights last">
-                <p>
-                    © 2014 Tisson WP Theme. All Rights Reserved.
+            <div class="content-container">
+               <img src="../../layout/img/beard.png" class="right parent" alt="">
+                <p >
+                    Все материалы взяты из открытых источников.
                 </p>			
             </div>
         </div>	
