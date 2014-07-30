cf976d211e1251311f54d0b9b5008606b9d64c3e
mentos <mentos.name@gmail.com>
2014-07-30 19:02:03 +0400

    Начал логику пули

--- application/assets/calc-form/4game.php
+++ application/assets/calc-form/4game.php
@@ -1,2 +1,137 @@
 <?php
 
+/* ==============================
+ *          ПЕРЕМЕННЫЕ
+ * ==============================
+ */
+
+// ============= ИГРОК 1 =============
+// гора
+$heap_p1 = $_POST['heap_player_1'];
+// пуля
+$pool_p1 = $_POST['pool_player_1'];
+// висты
+$p1Wp2 = $_POST['plr_1_whists_on_plr_2'];
+$p1Wp3 = $_POST['plr_1_whists_on_plr_3'];
+$p1Wp4 = $_POST['plr_1_whists_on_plr_4'];
+
+
+// ============= Игрок 2 =============
+// гора
+$heap_p2 = $_POST['heap_player_2'];
+// пуля
+$pool_p2 = $_POST['pool_player_2'];
+// висты
+$p2Wp1 = $_POST['plr_2_whists_on_plr_1'];
+$p2Wp3 = $_POST['plr_2_whists_on_plr_3'];
+$p2Wp4 = $_POST['plr_2_whists_on_plr_4'];
+
+
+// ============= Игрок 3 =============
+// гора
+$heap_p3 = $_POST['heap_player_3'];
+// пуля
+$pool_p3 = $_POST['pool_player_3'];
+// висты
+$p3Wp1 = $_POST['plr_3_whists_on_plr_1'];
+$p3Wp2 = $_POST['plr_3_whists_on_plr_2'];
+$p3Wp4 = $_POST['plr_3_whists_on_plr_4'];
+
+
+// ============= Игрок 4 =============
+// гора
+$heap_p4 = $_POST['heap_player_4'];
+// пуля
+$pool_p4 = $_POST['pool_player_4'];
+// висты
+$p4Wp1 = $_POST['plr_3_whists_on_plr_1'];
+$p4Wp2 = $_POST['plr_3_whists_on_plr_2'];
+$p4Wp3 = $_POST['plr_3_whists_on_plr_3'];
+
+
+/* ==============================
+ *           ЛОГИКА
+ * ==============================
+ */
+
+// помещаем горы в массив
+$heapArr = array(
+    "Игрок-1"=>$heap_p1,
+    "Игрок-2"=>$heap_p2,
+    "Игрок-3"=>$heap_p3,
+    "Игрок-4"=>$heap_p4
+);
+
+// определяем амнистёра
+$amnesty = min($heapArr);
+
+
+// узнаём кто это
+foreach ($heapArr as $player => $heapVal) {
+    if ($heapVal == $amnesty) {
+        $amnister = $player;
+    }
+}
+
+// уравниваем (амнистируем) гору у игроков
+$heapMeanArr = array();
+foreach ($heapArr as $heapValue){
+    if ($amnesty == $heapValue) {
+        array_push($heapMeanArr, $heapValue);
+    }
+    else {
+        $heapMean = $heapValue - $amnesty;
+        array_push($heapMeanArr, $heapMean);
+    }
+}
+
+// рассчитаем висты на игроков с их горы
+$whistValueArr = array();
+foreach ($heapMeanArr as $heapValue ) {
+
+    $whistValue = round(($heapValue * 10) / 3);
+    array_push($whistValueArr, $whistValue);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- application/views/view_four-player.php
+++ application/views/view_four-player.php
@@ -1,3 +1,4 @@
+<?php include_once "application/assets/calc-form/4game.php"; ?>
 <div class="content-container">
 
     <ol class="breadcrumb">
@@ -5,23 +6,25 @@
         <li class="active">Расчёт пули на четверых</li>
     </ol>
 
-    <form method="post" action="../assets/calc-form/4game.php" class="calculate-form">
+    <div class="panel panel-default pull-right hide">
+        <div class="panel-body">
+            <p class="text-center">Бланк пули на четверых<br>
+                <a class="btn btn-default btn-sm" href="../../files/bullet-4.doc" target="_blank"><i class="fa fa-download"></i> Скачать</a>
+                <a class="btn btn-default btn-sm" href="../../files/bullet-4.html" target="_blank"><i class="fa fa-print"></i> распечатать</a></p>
+        </div>
+    </div>
+
+    <form method="post" action="/four-player" class="calculate-form">
 
        <table class="table table-options">
            <tbody>
                <tr>
-                   <td rowspan="2">
-                       <h4>Бланк пули на четверых</h4>
-                        <p><a class="btn btn-default" href="../../files/bullet-4.doc" target="_blank"><i class="fa fa-download"></i> Скачать</a>
-                            <a class="btn btn-default" href="../../files/bullet-4.html" target="_blank"><i class="fa fa-print"></i> распечатать</a></p>
-                   </td>
-                   <td colspan="2">
-                        <h4>Настройки игры</h4>
+                   <td colspan="3">
+                        <h4>Настройка игры</h4>
                    </td>
                </tr>
                <tr>
                    <td>
-
                         <div class="radio">
                            <p><b>Делимость горы и пули</b></p>
                             <div class="checkbox">
@@ -45,14 +48,20 @@
                            <p><b>Стоимость в вистах очков горы и пули</b></p>
                             <div class="checkbox">
                                 <label>
-                                      <input type="radio" name="optionsCost" value="option1"> 1 пуля = 1 гора = 10 вистов
+                                      <input type="radio" name="optionsCost" value="option1"> 1 пуля = 1 гора = 10 вистов (Классика)
                                 </label>
                                 <br>
                                 <label>
-                                      <input type="radio" name="optionsCost" value="option2"> 1 пуля = 20 вистов, 1 гора = 10 вистов
+                                      <input type="radio" name="optionsCost" value="option2"> 1 пуля = 2 гора, 1 гора = 10 вистов (Ленинград)
                                 </label>
                             </div>
                         </div>
+                   </td>
+                   <td>
+                       <div class="form-group">
+                           <label for="exampleInputEmail1">Игра идёт до:</label>
+                           <input type="text" class="form-control" placeholder="Общая гора">
+                       </div>
                    </td>
                </tr>
            </tbody>
@@ -79,17 +88,17 @@
             <tbody>
                <tr>
                     <td><b>Гора</b></td>
-                    <td><input class="form-control" type="text" name="heap_player-1" placeholder="Гора"></td>
-                    <td><input class="form-control" type="text" name="heap_player-2" placeholder="Гора"></td>
-                    <td><input class="form-control" type="text" name="heap_player-3" placeholder="Гора"></td>
-                    <td><input class="form-control" type="text" name="heap_player-4" placeholder="Гора"></td>
+                    <td><input class="form-control" value="<?=$_POST['heap_player_1'];?>" type="text" name="heap_player_1" placeholder="Гора"></td>
+                    <td><input class="form-control" value="<?=$_POST['heap_player_2'];?>" type="text" name="heap_player_2" placeholder="Гора"></td>
+                    <td><input class="form-control" value="<?=$_POST['heap_player_3'];?>" type="text" name="heap_player_3" placeholder="Гора"></td>
+                    <td><input class="form-control" value="<?=$_POST['heap_player_4'];?>" type="text" name="heap_player_4" placeholder="Гора"></td>
                 </tr>
                 <tr>
                     <td><b>Пуля</b></td>
-                    <td><input class="form-control" type="text" name="pool_player-1" placeholder="Пуля"></td>
-                    <td><input class="form-control" type="text" name="pool_player-2" placeholder="Пуля"></td>
-                    <td><input class="form-control" type="text" name="pool_player-3" placeholder="Пуля"></td>
-                    <td><input class="form-control" type="text" name="pool_player-4" placeholder="Пуля"></td>
+                    <td><input class="form-control" value="<?=$_POST['pool_player_1'];?>" type="text" name="pool_player_1" placeholder="Пуля"></td>
+                    <td><input class="form-control" value="<?=$_POST['pool_player_2'];?>" type="text" name="pool_player_2" placeholder="Пуля"></td>
+                    <td><input class="form-control" value="<?=$_POST['pool_player_3'];?>" type="text" name="pool_player_3" placeholder="Пуля"></td>
+                    <td><input class="form-control" value="<?=$_POST['pool_player_4'];?>" type="text" name="pool_player_4" placeholder="Пуля"></td>
                 </tr>
                 <tr class="active">
                     <td colspan="5"><h5 class="text-center">Висты</h5></td>
@@ -97,49 +106,59 @@
                 <tr>
                     <td><b>Игрок 1</b></td>
                     <td class="disabled text-center text-danger"><i class="fa fa-times"></i></td>
-                    <td><input class="form-control" type="text" name="plr-1_whists-on-plr-2" placeholder="Висты на игрока 2"></td>
-                    <td><input class="form-control" type="text" name="plr-1_whists-on-plr-3" placeholder="Висты на игрока 3"></td>
-                    <td><input class="form-control" type="text" name="plr-1_whists-on-plr-4" placeholder="Висты на игрока 4"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_1_whists_on_plr_2'];?>" type="text" name="plr_1_whists_on_plr_2" placeholder="Висты на игрока 2"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_1_whists_on_plr_3'];?>" type="text" name="plr_1_whists_on_plr_3" placeholder="Висты на игрока 3"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_1_whists_on_plr_4'];?>" type="text" name="plr_1_whists_on_plr_4" placeholder="Висты на игрока 4"></td>
                 </tr>
                 <tr>
                    <td><b>Игрок 2</b></td>
-                    <td><input class="form-control" type="text" name="plr-2_whists-on-plr-1" placeholder="Висты на игрока 1"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_2_whists_on_plr_1'];?>" type="text" name="plr_2_whists_on_plr_1" placeholder="Висты на игрока 1"></td>
                     <td class="disabled text-center text-danger"><i class="fa fa-times"></i></td>
-                    <td><input class="form-control" type="text" name="plr-2_whists-on-plr-3" placeholder="Висты на игрока 3"></td>
-                    <td><input class="form-control" type="text" name="plr-2_whists-on-plr-4" placeholder="Висты на игрока 4"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_2_whists_on_plr_3'];?>" type="text" name="plr_2_whists_on_plr_3" placeholder="Висты на игрока 3"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_2_whists_on_plr_4'];?>" type="text" name="plr_2_whists_on_plr_4" placeholder="Висты на игрока 4"></td>
                 </tr>
                 <tr>
                    <td><b>Игрок 3</b></td>
-                    <td><input class="form-control" type="text" name="plr-3_whists-on-plr-1" placeholder="Висты на игрока 1"></td>
-                    <td><input class="form-control" type="text" name="plr-3_whists-on-plr-2" placeholder="Висты на игрока 2"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_3_whists_on_plr_1'];?>" type="text" name="plr_3_whists_on_plr_1" placeholder="Висты на игрока 1"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_3_whists_on_plr_2'];?>" type="text" name="plr_3_whists_on_plr_2" placeholder="Висты на игрока 2"></td>
                     <td class="disabled text-center text-danger"><i class="fa fa-times"></i></td>
-                    <td><input class="form-control" type="text" name="plr-3_whists-on-plr-4" placeholder="Висты на игрока 4"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_3_whists_on_plr_4'];?>" type="text" name="plr_3_whists_on_plr_4" placeholder="Висты на игрока 4"></td>
                 </tr>
                 <tr>
                    <td><b>Игрок 4</b></td>
-                    <td><input class="form-control" type="text" name="plr-4_whists-on-plr-1" placeholder="Висты на игрока 1"></td>
-                    <td><input class="form-control" type="text" name="plr-4_whists-on-plr-2" placeholder="Висты на игрока 2"></td>
-                    <td><input class="form-control" type="text" name="plr-4_whists-on-plr-3" placeholder="Висты на игрока 3"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_4_whists_on_plr_1'];?>" type="text" name="plr_4_whists_on_plr_1" placeholder="Висты на игрока 1"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_4_whists_on_plr_2'];?>" type="text" name="plr_4_whists_on_plr_2" placeholder="Висты на игрока 2"></td>
+                    <td><input class="form-control" value="<?=$_POST['plr_4_whists_on_plr_3'];?>" type="text" name="plr_4_whists_on_plr_3" placeholder="Висты на игрока 3"></td>
                     <td class="disabled text-center text-danger"><i class="fa fa-times"></i></td>
                 </tr>
 
-               <tr class="total">
-                    <td><h5>Итог</h5></td>
+               <tr class="total active">
+                    <td><h3>Итог</h3></td>
                     <td><span class="text-danger">- 265</span></td>
                     <td><span class="text-success">+ 354</span></td>
                     <td><span class="text-success">+ 132</span></td>
                     <td><span class="text-danger">- 221</span></td>
                 </tr>
             </tbody>
-            <tfoot class="hide">
+            <tfoot class="">
                 <tr>
                     <td colspan="5">
-                        <h4 class="text-center">Как вели подсчёт:
+                        <h3 class="text-center">Как вели подсчёт:
                             <span class="pull-right">
                                 <a id="collapse" class="text-info" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Показать <i class="fa fa-angle-down"></i></a></span>
-                        </h4>
-                        <div id="collapseOne" class="panel-collapse collapse">
-                            <p> Уравниваем пули. Поскольку пуля является «обратной стороной» горы, то, чтобы уравнять пули всех игроков нужно 12 списать с горы S. Но горы у всех нулевые. В этом случае списание 12 с горы S будет эквивалентно записью 12 в гору каждого игрока (т.е. W и E получают по 12 в свои горы). Теперь обратим их горы в висты умножением на 10 и делением на 3. Получаем 40. Это значит W должен 40 E и 40 S, в тоже время E должен 40 W и 40 S. W и E взаимно компенсируются, и каждый из них должен только S по 40. Таким образом, S выиграл 80, а W и E проиграли по 40.</p>
+                        </h3>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+
+                            <ul class="list-unstyled">
+                                <li><h4>Определяем амнистёра:</h4>
+                                    <ul class="list-unstyled">
+                                        <li><p>Это <b><?php echo $amnister; ?></b> с горой <b><?php echo $amnesty; ?></b></p></li>
+                                    </ul>
+                                </li>
+                                <li><h4>Уравниваем (амнистируем) гору у игроков</h4></li>
+                                <li></li>
+                            </ul>
+
                         </div>
                     </td>
                 </tr>
@@ -149,4 +168,16 @@
         <button id="submit" type="submit" class="btn btn-primary btn-lg">Спустить курок !</button>
 
     </form>
+    <br>
+    <pre>
+
+        <?php
+            print_r($amnesty);
+        ?>
+
+    </pre>
+
+    <p class="bs-callout bs-callout-info">В форме допускается использовать ТОЛЬКО цифры. <i class="fa fa-warning fa-2x pull-right"></i> </p>
+
+
 </div>
